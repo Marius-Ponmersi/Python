27e78761e8f7ff51b3cb463513a4d1124a2edcea
Marius-Ponmersi <80091621+Marius-Ponmersi@users.noreply.github.com>
2021-04-23 23:25:47 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,11 +1,17 @@
+# Работает только с нечетным количеством элементов списка
 spisok = input("Введите через пробел элементы списка: ").split()
 print(spisok)
-a = 0
-n = len(spisok)
+print(len(spisok))
+new = ''
 
-for el in spisok:
-    if a < n:
-        spisok.insert(a + 1, el[a])
-    a += 2
-print(spisok)
+for el in range(len(spisok)):
+    el = 0
+    while el < (len(spisok) - 1):
+        tmp = spisok[el]
+        spisok[el] = spisok[el + 1]
+        spisok[el + 1] = tmp
+        el = el + 2
 
+new = ''.join(spisok)
+new_spisok = list(new)
+print(new_spisok)
